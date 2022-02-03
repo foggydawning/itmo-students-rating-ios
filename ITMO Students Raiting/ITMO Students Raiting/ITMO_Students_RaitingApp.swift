60ec4dd57976ac9f73a37ca960bb3239fbac22a1
foggydawning <foggydawning@gmail.com>
2022-02-03 13:18:15 +0300

    create xcodeproj

---
+++ ITMO Students Raiting/ITMO Students Raiting/ITMO_Students_RaitingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITMO_Students_RaitingApp.swift
+//  ITMO Students Raiting
+//
+//  Created by Илья Чуб on 03.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ITMO_Students_RaitingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
